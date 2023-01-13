de0c86beb78d4f34c4d0bcd4993ba41b091378bb
Marko Bolowski <marko.bolowski@stud.uni-hannover.de>
2023-01-13 15:40:40 +0100

    detailView: back button function added

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-$(function(){
+$(function () {
 });
 function login() {
-      var tmp = document.getElementById('login').innerHTML;
-      if (tmp == "Ausloggen") {
-        document.getElementById('login').innerHTML = "Einloggen";
-      } else {
-        document.getElementById('login').innerHTML = "Ausloggen";
-      }
+  var tmp = document.getElementById('login').innerHTML;
+  if (tmp == "Ausloggen") {
+    document.getElementById('login').innerHTML = "Einloggen";
+  } else {
+    document.getElementById('login').innerHTML = "Ausloggen";
+  }
+}
+
+function updateShoppingCart(amount) {
+  if (amount == 0) {
+    document.getElementById('shoppingCart').src = "img/shoppingcart.jpeg";
+  } else if (amount == 1) {
+    document.getElementById('shoppingCart').src = "img/shoppingcart_1.jpeg";
+  } else if (amount == 2) {
+    document.getElementById('shoppingCart').src = "img/shoppingcart_2.jpeg";
+  } else if (amount == 3) {
+    document.getElementById('shoppingCart').src = "img/shoppingcart_3.jpeg";
+  } else {
+    document.getElementById('shoppingCart').src = "img/shoppingcart_x.jpeg";
+  }
+}
+
+
+var selectedProduct = null
+
+function showDetailView(product) {
+  $(function () {
+    if (selectedProduct) {
+      $("#" + selectedProduct).css("display", "none");
     }
+    $("#" + product).css("display", "inline");
+    $(".overview-grid").css("display", "none");
+    selectedProduct = product;
+  });
+}
 
-    function updateShoppingCart(amount) {
-      if (amount == 0) {
-        document.getElementById('shoppingCart').src = "img/shoppingcart.jpeg";
-      } else if (amount == 1) {
-        document.getElementById('shoppingCart').src = "img/shoppingcart_1.jpeg";
-      } else if (amount == 2) {
-        document.getElementById('shoppingCart').src = "img/shoppingcart_2.jpeg";
-      } else if (amount == 3) {
-        document.getElementById('shoppingCart').src = "img/shoppingcart_3.jpeg";
-      } else {
-        document.getElementById('shoppingCart').src = "img/shoppingcart_x.jpeg";
-      }
-    }
-
-    
-    var selectedProduct = null
-
-    function showDetailView(product) {
-      $(function () {
-        if (selectedProduct) {
-          $("#" + selectedProduct).css("visibility", "hidden");
-        }
-        $("#" + product).css("visibility", "visible");
-        $(".overview-grid").css("display", "none");
-        selectedProduct = product;
-      });
-    }
-
-    $(window).on("message", function (m) {
-      $("#shoppingCart").click(function () {
-        console.log("Clicked: ");
-      });
-      const message = JSON.parse(m.originalEvent.data);
-      $(message.item).attr(message.attr, message.value);
-    });
+$(window).on("message", function (m) {
+  $("#shoppingCart").click(function () {
+    console.log("Clicked: ");
+  });
+  const message = JSON.parse(m.originalEvent.data);
+  if (message.attr == "style") {
+    const prop = message.value.split(":")[0]
+    const value = message.value.split(":")[1]
+    $(message.item).css(prop, value);
+  } else {
+    $(message.item).attr(message.attr, message.value);
+  }
+});
